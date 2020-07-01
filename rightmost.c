9365fd49c56bb0306e9fc5873f734af8d7034421
Khalid Beneddine <khaledbeneddine@gmail.com>
2020-07-01 14:08:00 +0200

    add the rightmost program

--- rightmost.c
+++ rightmost.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
+#define MAXLINE 1000
 
-#define MAXLINE 1000 /* maximum input line */
-
-int getline(char s[], int max);
-int strindex(char s[], char t[]);
+int getline(char s[], int lim);
+int rightmost(char s[], char t[]);
 
 char pattren[] = "ould"; /* pattren to search for */
 
-/* find all lines matching with pattren */
-int main(void) 
+/* find all lines maching with pattren, then print the rightmost occurence of t in s */
+int main(void)
 {
     char line[MAXLINE];
     int found = 0;
     while (getline(line, MAXLINE) > 0)
     {
-        if (strindex(line, pattren) >= 0)
+        if (rightmost(line, pattren) >= 0)
         {
-            printf("%s\n", line);
-            found++;
+            printf("%s\nrightmost index : %i\n", line, rightmost(line, pattren));
         }
     }
-    return found;
+    return 0;
 }
 
 /* getline: get line into s, return the length */
@@ -42,17 +38,19 @@ int getline(char s[], int lim)
     return i;
 }
 
-/* strindex: return index of t in s, -1 if none */
-int strindex(char s[], char t[])
+/* rightmost: return the rightmost occurence index of t in s, -1 if none */
+int rightmost(char s[], char t[])
 {
     int i, j, k;
+    int index = -1;
     for (i = 0; s[i] != '\0'; i++)
     {
         for (j = i, k = 0; t[k] != '\n' && s[j] == t[k]; j++, k++);
         if (k > 0 && t[k] == '\0')
         {
-            return i;
+            index = i;
+            continue;
         }
     }
-    return -1;
+    return index;
 }
