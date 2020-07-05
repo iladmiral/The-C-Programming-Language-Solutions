9e7e8823ae43cefcafb8f1306db9b78f26ff77cb
Khalid Beneddine <khaledbeneddine@gmail.com>
2020-07-05 15:46:33 +0200

    Add stack

--- polishNotation/stack.c
+++ polishNotation/stack.c
@@ -1,5 +1,24 @@
-#include <ctype.h>
+#define MAXVAL 100 /* maximum depth of val stack */
 
-int getch(void);
-void ungetch(int);
+int sp = 0;     /* next free stack position */
+int val[MAXVAL]; /* value stack */
+
+/* push: push f into value stack */
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return the top value of stack */
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else
+        printf("error: stack is empty\n");
+        return 0.0;
+}
 
